0bfa92b314a1d5993f36b543c22e056da94e86d7
Sivnaveen L. <sivanaveenecemec@gmail.com>
2024-08-08 10:52:46 +0530

    Create DemoSet1.java

---
+++ DemoSet1.java
@@ -0,0 +1,26 @@
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
+public class DemoSet1
+{
+public static void main(String[]args)
+{
+Set<Integer> s=new HashSet<Integer>();
+s.add(29);
+s.add(45);
+s.add(null);
+s.add(null);
+s.add(67);
+s.add(67);
+s.add(52);
+s.add(78);
+s.add(90);
+System.out.println(s);
+System.out.println(s.remove(67));
+System.out.println(s);
+Iterator<Integer> i=s.iterator();
+while(i.hasNext())
+       System.out.println(i.next()+" ");
+}
+}
